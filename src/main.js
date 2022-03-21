af21a57be3972c1ba03a48212d3789ba630aa61c
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-03-20 21:30:04 -0500

    Listando reportes  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/main.js 	modified:   src/router/index.js 	deleted:    src/views/AboutView.vue 	new file:   src/views/ReportView.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import Router from './router'
+import Store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const app = createApp(App)
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(Store)
+app.use(Router)
+app.use(VueAxios, axios)
+app.mount('#app')
+//variables globales
+const uri ={
+    local : 'http://localhost:8000'
+}
+
+//Nombre de la app
+app.config.globalProperties.$app_name = 'Tkambio'
+//Uri 
+app.config.globalProperties.$uri = uri.local
---
+++ src/views/ReportView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="about">
+    <h1>Generador de reportes TK</h1>
+      <table>
+        <thead>
+          <th>Título</th>
+          <th>Fecha de creación</th>
+          <th>Acción</th>
+        </thead>
+        <tbody>
+          <tr v-for="report in listReports" v-bind:key="report.id">
+            <td>{{report.title}}</td>
+            <td>{{report.created_at}}</td>
+            <td><a href="#"> Descargar</a> </td>
+          </tr>
+        </tbody>
+      </table>
+      <hr>
+      <button>Crear Reporte</button>
+  </div>
+</template>
+<script>
+
+
+  export default {
+
+    data() {
+        return {
+          listReports:[]
+        }
+    },
+    created() {
+      
+    },
+
+    mounted() {
+      this.reportList()
+    },
+
+    components:{
+
+    },
+
+    methods: {
+      reportList(){
+
+        this.axios.get(this.$uri+'/api/list-reports')
+        .then(response => {
+            console.log(response.data.response)
+            this.listReports = response.data.response.data 
+        })
+        .catch(error => {
+
+        })
+        .finally(() => {
+
+        })
+      }
+    },
+  }
+</script>
